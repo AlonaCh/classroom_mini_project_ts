3dd75eb58ab1020865cc89477b804c8448bce3a3
Alona Chubenko <aliona.chubenko@gmail.com>
2024-03-05 11:26:12 +0200

    Start of the project

---
+++ src/classroom.ts
@@ -0,0 +1,26 @@
+//`getNumberOfGrades` should return the number of grades
+export function getNumberOfGrades(grades) {
+    
+}
+
+//`getSumGrades` should return the sum of all the grades
+export function getSumGrades(grades) {
+    
+}
+//`getAverageValue` should return the average value of all grades (the sum of all grades divided by the total number of grades).
+export function getAverageValue(grades) {
+    
+}
+//`getPassingGrades` should return all passing grades (10 and above).
+export function getPassingGrades(grades) {
+   
+}
+
+//`getFailingGrades` should return all failing grades (9 and below).
+export function getFailingGrades(grades) {
+    
+}
+//`getRaisedGrades` should return all the grades raised by 1 (grades should not exceed 20).
+export function getRaisedGrades(grades) {
+    
+}
